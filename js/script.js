88c8d799f466e33200126bb3324b9833a1215d0c
Mari <mari-jen@hotmail.com>
2022-07-18 20:17:43 -0300

    teste

--- js/script.js
+++ js/script.js
@@ -27,8 +27,8 @@ modal.init();
 const tooltip = new Tooltip('[data-tooltip]');
 tooltip.init();
 
-// const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
-// scrollAnima.init();
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
 
 const dropdownMenu = new DropdownMenu('[data-dropdown]');
 dropdownMenu.init();
